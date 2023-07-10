5254270e19614da214dd792dc15504c9f28cd605
mayurrkukreja <mayurkukreja02@gmail.com>
2023-07-11 02:20:08 +0530

    merge API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
 
     useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch('https://workout-planner-backend.onrender.com/api/workouts')
+            const response = await fetch('https://workout-planner-frontend.vercel.app/api/workouts')
             const json = await response.json()
             console.log(json)
 
